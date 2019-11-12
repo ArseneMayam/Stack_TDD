a7279c704c1501243ed0811f5fd3ac7acd1b76b1
Mayam <arsene.mayam@cgi.com>
2019-11-12 13:54:21 -0500

    Initial commit

---
+++ Stack/CustomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class CustomException 
+    {
+        public class ExceededSizeException : Exception
+        {
+
+        }
+
+        public class ExpenditureProhibitedException : Exception
+        {
+
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Stack.CustomException;
+
+namespace Stack
+{
+   public  class Stack<T>
+    {
+        #region Members
+        private readonly T[]  stackArray;
+        private readonly  int maximumLength;
+        #endregion
+
+        #region Properties
+        public int Size { get; private set; }
+        #endregion
+
+        #region Constuctor
+        public Stack(int length)
+        {
+            maximumLength = length;
+            stackArray = new T[length];
+        }
+        #endregion
+
+        public void Push(T value)
+        {
+            if(Size == maximumLength)
+                throw new ExceededSizeException();
+
+            stackArray[Size++] = value;
+        }
+        public T Pop()
+        {
+            if (Size == 0)
+                throw new ExpenditureProhibitedException();
+
+            return stackArray[--Size];
+        }
+        public T Peek()
+        {
+            if (Size == 0)
+                throw new ExpenditureProhibitedException();
+            return stackArray[Size - 1];
+        }
+    }
+}
